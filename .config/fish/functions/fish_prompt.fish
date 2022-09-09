04c76e6be6b2ec18a74bfd102333439b62739634
hiroshi-tarumi <tarumi8171@gmail.com>
2022-09-09 23:14:32 +0900

    test

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -37,7 +37,7 @@ function fish_prompt --description 'Write out the prompt'
     
     end
 
-    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    echo -s (prompt_login) '$#$' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal '#$#' $prompt_status $suffix
     echo -n -s $status_color $suffix ' <> ' $normal
 
 end
